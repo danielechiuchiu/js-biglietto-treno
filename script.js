583123d92817f8f78ff3c4f8f6c1437d5c33f597
danielechiuchiu <daniele.chiuchiu@gmail.com>
2020-07-07 15:41:25 +0200

    final commit

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ var eta = parseInt (prompt("Inserisci la tua età:"));
 var risultato = km * 0.21;
 
 if (eta < 18) {
-document.getElementById("costo-treno").innerHTML = "il costo è: &nbsp" + risultato * 0.8;
+document.getElementById("costo-treno").innerHTML = "Il costo è: &nbsp" + risultato * 0.8 + "&nbspEur";
 }
 
 else if(eta >= 18){
-document.getElementById("costo-treno").innerHTML = "il costo è: &nbsp" + risultato;
+document.getElementById("costo-treno").innerHTML = "Il costo è: &nbsp" + risultato + "&nbspEur";
 }
 
 if (eta >= 65) {
-document.getElementById("costo-treno").innerHTML = "il costo è: &nbsp" + risultato * 0.6;
+document.getElementById("costo-treno").innerHTML = "Il costo è: &nbsp" + risultato * 0.6 + "&nbspEur";
 }
